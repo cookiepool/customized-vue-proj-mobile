6d78444816509b64c97211cac13a1370393ba065
cookiepool <18381317534@163.com>
2020-01-06 15:35:31 +0800

    update

--- src/views/RouterTest.vue
+++ src/views/RouterTest.vue
@@ -4,6 +4,7 @@
 
     <p class="text">路由测试</p>
     <p class="text-lorem">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem nobis excepturi unde praesentium voluptatum dolorum iure odit expedita temporibus, nemo sequi delectus saepe commodi, a, mollitia vel asperiores architecto aperiam.</p>
+    <h1>{{ msg == '' ? 'msg': msg }}</h1>
   </div>  
 </template>
 
@@ -14,8 +15,13 @@ export default {
   name: 'RouterTest',
   data() {
     return {
-
+      msg: ''
     }
+  },
+  mounted() {
+    setTimeout(() => {
+      this.msg = 'hello'
+    }, 3000)
   },
   components: {
     HeadCommon
